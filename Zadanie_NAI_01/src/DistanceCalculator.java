36c90d4ab62cf1c54b2c08cc47245d6167219c01
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-03-16 19:43:21 +0100

    Wstępna logika klasy - DistanceCalculator.java

--- Zadanie_NAI_01/src/DistanceCalculator.java
+++ Zadanie_NAI_01/src/DistanceCalculator.java
@@ -1,3 +1,12 @@
 public class DistanceCalculator {
-    
+    public static double calculateDistance(double[] point1, double[] point2) {
+
+        if (point1 == null || point2 == null) {
+            throw new IllegalArgumentException("Punkty nie mogą być nullem.");
+        }
+
+        if (point1.length != point2.length) {
+            throw new IllegalArgumentException("Punkty muszą mieć tyle samo wymiarów.");
+        }
+    }
 }
