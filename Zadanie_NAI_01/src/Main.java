87041fec0adf816ee8e2357b289f4720990a16ce
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-03-16 19:07:29 +0100

    Rozpoczęcie projektu - Main.java

---
+++ Zadanie_NAI_01/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double[] p1 = {1, 2, 3};
+        double[] p2 = {4, 6, 8};
+
+        System.out.println("Odległość: " + DistanceCalculator.calculateDistance(p1, p2));
+    }
+}
