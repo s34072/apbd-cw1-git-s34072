3e6300f2bc28197ac302b8a414234b084e6c00b3
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-03-16 19:44:29 +0100

    Skończona klasa - DistanceCalculator.java

--- Zadanie_NAI_01/src/DistanceCalculator.java
+++ Zadanie_NAI_01/src/DistanceCalculator.java
@@ -8,5 +8,13 @@ public class DistanceCalculator {
         if (point1.length != point2.length) {
             throw new IllegalArgumentException("Punkty muszą mieć tyle samo wymiarów.");
         }
+        double sum = 0.0;
+
+        for (int i = 0; i < point1.length; i++) {
+            double difference = point1[i] - point2[i];
+            sum += Math.pow(difference, 2);
+        }
+
+        return Math.sqrt(sum);
     }
 }
